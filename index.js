7e9ea7ca918f4f6c2b61c0f1d522750711bc916d
Phil <philrsheridan@gmail.com>
2023-10-19 16:55:32 -0400

    passed 3 tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName, userEmail){
+
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            document.body.append(object.id);
+        })
+        .catch(function(error) {
+            alert("failed POST request");
+            document.body.append(error.message);
+        });
+}
